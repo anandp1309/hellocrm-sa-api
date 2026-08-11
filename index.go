e053523387f8150ed7e432a6c34ef60148174bfb
Nikhil Jahagirdar <nikhil.jahagirdar@gmail.com>
2026-08-11 13:38:43 +0530

    vercel setup

---
+++ index.go
@@ -0,0 +1,23 @@
+package api
+
+import (
+	"net/http"
+
+	"hellocrm-superadmin/internal/server"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/labstack/echo/v4"
+)
+
+var e *echo.Echo
+var dbPool *pgxpool.Pool
+
+func init() {
+	// Initialize the app once per lambda container
+	e, dbPool = server.BuildApp()
+}
+
+// Handler is the serverless entrypoint for Vercel
+func Handler(w http.ResponseWriter, r *http.Request) {
+	e.ServeHTTP(w, r)
+}
